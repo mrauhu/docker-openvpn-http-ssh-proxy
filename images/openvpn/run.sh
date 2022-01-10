ff2c224e2afde0e5858f8366befd6baf42c7c141
Mrau Hu <mrauhu@yandex.ru>
2022-01-10 20:04:19 +0300

    Auto auth using username and password in `openvpn/run.sh`

--- images/openvpn/run.sh
+++ images/openvpn/run.sh
@@ -10,14 +10,32 @@ echo $gw
 
 ls -la
 
-MERGED_CONFIG=merged.config.ovpn
+# Enable compatability layer with Alpine
+MERGED_CONFIG_FILE=merged.config.ovpn
 
-cat <<EOF > $MERGED_CONFIG
+cat <<EOF > $MERGED_CONFIG_FILE
 script-security 2
 up /etc/openvpn/up.sh
 down /etc/openvpn/down.sh
 EOF
 
-cat "${OPENVPN_CONFIG_FILE}" >> $MERGED_CONFIG
+cat "${OPENVPN_CONFIG_FILE}" >> $MERGED_CONFIG_FILE
 
-openvpn --config "/opt/openvpn/${MERGED_CONFIG}"
+# Auth using username and password
+VPN_AUTH_FILE=vpn.auth
+# Create credentials file
+cat <<EOF > $VPN_AUTH_FILE
+${OPENVPN_USERNAME}
+${OPENVPN_PASSWORD}
+EOF
+
+# Add `auth-user-pass $VPN_AUTH_FILE` to the merged config
+if grep -q auth-user-pass $MERGED_CONFIG_FILE; then
+  # if has string started with `auth-user-pass`
+  sed -i "s/^auth-user-pass.*/auth-user-pass ${VPN_AUTH_FILE}/" $MERGED_CONFIG_FILE
+elif [ -n "${OPENVPN_USERNAME}" ]; then
+   # If not empty username
+  echo "auth-user-pass ${VPN_AUTH_FILE}" >> $MERGED_CONFIG_FILE
+fi
+
+openvpn --config "/opt/openvpn/${MERGED_CONFIG_FILE}"
